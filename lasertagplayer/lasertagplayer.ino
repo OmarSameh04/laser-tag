9d65b387e1999fb18d800e30fb3bbde6b8a7c818
Omar Sameh <160317727+OmarSameh04@users.noreply.github.com>
2025-05-17 23:43:13 +0300

    finalised functionality

--- lasertagplayer/lasertagplayer.ino
+++ lasertagplayer/lasertagplayer.ino
@@ -1,21 +1,36 @@
-#include <Wire.h>
+#include <MFRC522.h>
+#include <SPI.h>
 #include <LiquidCrystal_I2C.h>
+#include <WiFi.h>
 
 
 #define chest_1 36 
 #define chest_2 39
 #define chest_3 34
 #define chest_4 35
-#define Calibrated 4
+#define back_1 32
+#define back_2 33
 #define trigger 13
 #define laser 14
+#define SCK 18
+#define MOSI 23
+#define MISO 19
+#define RST 4
+#define SS 5    // SDA
+
+const char* ssid = "Sameh";
+const char* password = "123456SR";
 const int chest[] = {chest_1, chest_2, chest_3, chest_4};
 bool shot = false;
-LiquidCrystal_I2C lcd(0x27, 16, 2);
+bool hasShot = false;
 int life = 10;
 int ammo = 10;
 
- byte heart[8] = {
+WiFiClient client;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+MFRC522 mfrc522(SS, RST);
+
+byte heart[8] = {
   B00000,
   B00000,
   B11011,
@@ -26,7 +41,7 @@ int ammo = 10;
   B00000
 };
 
-// Custom bullet character
+
 byte bullet[8] = {
   B00100,
   B01110,
@@ -39,52 +54,118 @@ byte bullet[8] = {
 };
 
 void setup() {
-  Serial.begin(115200);
+ // Serial.begin(115200);
+  SPI.begin();        
+  mfrc522.PCD_Init();
+  WiFi.begin(ssid,password);
   lcd.init();
-  lcd.backlight();
+  lcd.createChar(0, heart);
+  lcd.createChar(1, bullet);
+  lcd.backlight(); 
   pinMode(chest_1,INPUT);
   pinMode(chest_2,INPUT);
   pinMode(chest_3,INPUT);
   pinMode(chest_4,INPUT);
-  pinMode(Calibrated, OUTPUT);
+  pinMode(back_1, INPUT);
+  pinMode(back_2, INPUT);
   pinMode(trigger,INPUT);
   pinMode(laser,OUTPUT);
-  //while(!isCalibrated());
-  digitalWrite(Calibrated, HIGH);
-  delay(1000);
-
-  lcd.createChar(0, heart);
-  lcd.createChar(1, bullet); 
-  updateLCD();
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(700);
+    lcd.setCursor(0,0);
+    lcd.print("CONNECTING");
+  }
+  if (client.connect("192.168.63.78", 1234)) {   
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(2,0);
+    lcd.print("READY TO PLAY!");
+    lcd.setCursor(3,1);
+    lcd.print("LASER TAG!");
+    client.println("HP:" + String(life) + "|AMMO:" + String(ammo));
+    delay(2000);
+    updateLCD();
+  }
+  else {
+    lcd.setCursor(0,0);
+    lcd.print("CONN. FAILED");
+  }
 }
 
 void loop() {
   //shooting code
-  char trig1 = digitalRead(trigger);
-  delay(25);
+  //char trig1 = digitalRead(trigger);
+  //delay(10);
   char trig2 = digitalRead(trigger);
-  if ((trig2 == HIGH)&&(trig1==LOW)) {  // Button pressed (assuming pull-up)
+  if(trig2 == HIGH && hasShot){
+  isShot();
+  hasShot = false;
+  delay(25);
+  }
+  if ((trig2 == HIGH) and !hasShot) {  // Button pressed (assuming pull-down)
     if (ammo > 0){
-     ammo--;
-     digitalWrite(laser,HIGH);
+      ammo--;
+      //if (client.connected()) 
+      client.println("HP:" + String(life) + "|AMMO:" + String(ammo));
+      digitalWrite(laser,HIGH);
     }
     updateLCD();
-    delay(300);
-    digitalWrite(laser,LOW);  // Debounce
+    hasShot = true;
+    delay(50);
+    isShot();
+    bool shot1 = shot;
+    delay(50);
+    isShot();
+    bool shot2 = shot;
+    delay(50);
+    isShot();
+    bool shot3 = shot;
+    delay(50);
+    isShot();
+    bool shot4 = shot;
+    digitalWrite(laser,LOW);
+    if(shot1 || shot2 || shot3 || shot4)
+    shot = true;
   }
   //shot code
   isShot();
   if(shot){
     life--;
+    lcd.setCursor(0,0);
+    lcd.print("                ");
+    lcd.setCursor(4,0);
+    lcd.print("OUCH!");    
+    if (client.connected()) {
+      client.println("HP:" + String(life) + "|AMMO:" + String(ammo));
+    }    
     if(life==0){
       //endgame logic
     }
     else{
       shot = false;
+      delay(300);
       updateLCD();
     }
-    delay(300);
   }
+  //reload code
+  if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,0);
+    lcd.print("Reloading...");
+    delay(300);
+    lcd.setCursor(0,0);
+    lcd.print("Reloading....");
+    delay(300);
+    lcd.setCursor(0,0);
+    lcd.print("Reloading.....");
+    delay(300);
+    lcd.setCursor(0,0);
+    lcd.print("Reloading......");
+    delay(300);
+    ammo = 10;
+    updateLCD();
+  } 
 }
 
 void updateLCD() {
@@ -130,6 +211,4 @@ void isShot(){
   }
   if(min1<0.8*min2)
     shot=true;
-  else
-    Serial.println("no");
 }
